f5034c16df2b89644bc2d24c8cca868d2e3341d6
Drew Bolero <andrew-bolero@uiowa.edu>
2017-03-06 15:11:30 -0600

    added model, dbConn, and ojdbc5

---
+++ src/java/model/Teams.java
@@ -0,0 +1,85 @@
+
+package model;
+
+
+public class Teams {
+
+    private int teamID;
+    private String teamName;
+    private String sport;
+    private int firstYear;
+    private int championships;
+    private String coach;
+    
+    public Teams() {
+        this.teamID = 0;
+        this.teamName = "";
+        this.sport = "";
+        this.firstYear = 0;
+        this.championships = 0;
+        this.coach = "";
+    }
+
+    public Teams(int teamID, String teamName, String sport, int firstYear, int championships, String coach) {
+        this.teamID = teamID;
+        this.teamName = teamName;
+        this.sport = sport;
+        this.firstYear = firstYear;
+        this.championships = championships;
+        this.coach = coach;
+    }
+
+    public int getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(int teamID) {
+        this.teamID = teamID;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public int getFirstYear() {
+        return firstYear;
+    }
+
+    public void setFirstYear(int firstYear) {
+        this.firstYear = firstYear;
+    }
+
+    public int getChampionships() {
+        return championships;
+    }
+
+    public void setChampionships(int championships) {
+        this.championships = championships;
+    }
+
+    public String getCoach() {
+        return coach;
+    }
+
+    public void setCoach(String coach) {
+        this.coach = coach;
+    }
+
+    @Override
+    public String toString() {
+        return "Teams{" + "teamID=" + teamID + ", teamName=" + teamName + ", sport=" + sport + ", firstYear=" + firstYear + ", championships=" + championships + ", coach=" + coach + '}';
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : index
+    Created on : Mar 6, 2017, 1:37:59 PM
+    Author     : Drew
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Chicago Pro Teams</title>
+    </head>
+    <body>
+        <h1>Chicago Pro Teams Database</h1>
+        
+        <a href="read">View All Teams</a>
+    </body>
+</html>
